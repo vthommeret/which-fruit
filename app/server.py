216d01da95f00167a03c34f06c5d931cb386a2ec
Vernon Thommeret <synotic@gmail.com>
2020-05-06 09:28:28 -0400

    Swap model

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1511aTQzfiaJVqYWb-XDxHG7zBbuZ3T9Q'
+export_file_name = 'which-fruit.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['banana', 'durian', 'jackfruit', 'mango', 'orange', 'pear']
 path = Path(__file__).parent
 
 app = Starlette()
